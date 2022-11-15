4751cf4595ffe806a24e209ca72a26d8d01acdab
Guilherme <antonioguilhermeinfo@gmail.com>
2022-11-15 17:31:21 -0300

    resolvendo erro de quando chegava em cima

--- pac-men/PersonagePhysics.js
+++ pac-men/PersonagePhysics.js
@@ -1,48 +1,17 @@
 class PersonagePhysics {
     move(personage,world,deltaTime) {
 
-        let personageColumn = 0.0
-        let personageRow = 0.0
+        let personageColumn = 0
+        let personageRow = 0
 
         personageColumn = Math.floor((personage.positionX )/world.scenery.width);
         personageRow = Math.floor(personage.positionY/world.scenery.height);
 
 
-        //personageColision
-
-        /*
-        dividir a posicao do personagem pela sua respectiva orientação (eixo x ou eixo y)
-        nos dá uma posicao exata de em qual linha ou coluna ele está, porém ele está se
-        orientando pelo canto superior esquerdo, o quer dizer se ele, o personagem, vinher
-        da direita, mesmo metade dele já estando na coluna onde se tem um cenário, pelo fato
-        de está orientado pelo canto superior esquerdo, esse canto ainda não terá passado para
-        a outra coluna, fazendo com que não haja choque preciso, assim é preciso verificar o 
-        último movimento, que se for right, deverá-se somar 60 a posicao x para que ele ele se
-        oriente agora pelo ponto superior direito e não mais pelo ponto superior esquerdo, e se
-        ele vinher da esquerda, deixa normal
-        */
-
         if (personageColumn < 0) personageColumn = 0
         if (personageRow < 0) personageRow = 0
 
-        // let positionXObstaculo = personageColumn * 60
-        // let positionYObstaculo = personageRow * 60
-        // let verifyColision = false
-        
         let choqueScenery = world.scenery.grid[personageRow][personageColumn] == 0 ? true : false
-        // console.log(world.scenery.grid[personageRow][personageColumn])
-
-
-        /*
-                world.scenery.grid[personageRow - 1][personageColumn] == 0
-                    && personage.positionY <= leftTopY
-
-                    verifica se na linha anterior na mesma coluna  a posicao y do personagem ,que 
-                    se refere a posicao esquerda de x, é menor que a posicao do obstaculo de y, só
-                    que inferior
-
-       */
-
 
         //irmaozinho da direita
         let cenarioLeftX = (personageColumn + 1)  * (world.scenery.width)  
@@ -62,16 +31,11 @@ class PersonagePhysics {
         let personagePositionLeftX = personage.positionX
         let personagePositionRightX = (personage.positionX) + world.personage.width
 
-            document.getElementById("alo").innerText = " \n personage right: "+
-            personagePositionRightX +"\n cenario Left: "+cenarioLeftX +
-           "\ntem cenario em cima :"+ world.scenery.grid[personageRow - 1][personageColumn] +
-           "\ntem cenario em cima na direita :"+ world.scenery.grid[personageRow - 1][personageColumn + 1] 
-
         switch(KeyBoardInput.state) {
             case "UP":    
-                if ((parseInt(world.scenery.grid[personageRow - 1][personageColumn]) == parseInt(0)
+                if ((parseInt(world.scenery.grid[personageRow - 1 == -1 ? 0 : personageRow - 1][personageColumn]) == parseInt(0)
                     && parseFloat(personagePositionTopY)  <= parseFloat(cenarioBottomY) 
-                    || (parseInt(world.scenery.grid[personageRow - 1][personageColumn + 1]) == parseInt(0) &&
+                    || (parseInt(world.scenery.grid[personageRow - 1 == -1 ? 0 : personageRow - 1][personageColumn + 1]) == parseInt(0) &&
                         parseFloat(personagePositionRightX)  >= parseFloat(cenarioLeftX))))  {
                         
                 } else {
@@ -81,7 +45,7 @@ class PersonagePhysics {
                 }
                 break;
             case "DOWN":
-                if (personage.positionY < (World.canvas.height - 60)) {
+                if (personage.positionY < (World.canvas.height - world.personage.width)) {
                     personage.positionY += personage.speed;
                 }
                 break;
@@ -91,11 +55,13 @@ class PersonagePhysics {
                 }
                 break;
             case "RIGHT":
-                if (personage.positionX < (World.canvas.width - 60)) {
+                if (personage.positionX < (World.canvas.width - world.personage.width)) {
                     personage.positionX += personage.speed;
                 }
                 break;
         }
+ 
+     
     
 
         // var text = "posicao esquerda :\n" + "1"+ "2"
