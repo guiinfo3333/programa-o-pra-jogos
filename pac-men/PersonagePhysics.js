4c879bf00be883c9f24323baa00c908d66c65432
Guilherme <antonioguilhermeinfo@gmail.com>
2022-11-22 16:09:07 -0300

    tentando ajeitar colisao

--- pac-men/PersonagePhysics.js
+++ pac-men/PersonagePhysics.js
@@ -1,85 +1,81 @@
 class PersonagePhysics {
     move(personage,world,deltaTime) {
 
-        let personageColumn = 0
-        let personageRow = 0
+        var res = this.calcChoques(personage.positionX, personage.positionY, personage.width, personage.height, world)
+        let choqueScenerySuperiorEsquerdoX = res[0]
+        let choqueScenerySuperiorDireitoX = res[1]
+        let choqueScenerySuperiorCimaY = res[2]
+        let choqueScenerySuperiorBaixoY = res[3]
 
-        personageColumn = Math.floor((personage.positionX )/world.scenery.width);
-        personageRow = Math.floor(personage.positionY/world.scenery.height);
+
+        document.getElementById("alo").innerText = "superior esquerdo  X" +choqueScenerySuperiorEsquerdoX+
+        "\n superior direito X"+ choqueScenerySuperiorDireitoX +
+        "\n superior cima Y" + choqueScenerySuperiorCimaY +
+        "\n superior baixo Y" + choqueScenerySuperiorBaixoY +
+        "\ posicao x esquerdo do personagem" + personage.positionX
+
+
+
+        switch(KeyBoardInput.state) {
+            case "UP":    
+                if (!choqueScenerySuperiorCimaY)  {
+                    if (personage.positionY >= 0) {
+                        personage.positionY -= personage.speed; 
+                    }
+                } else {
+                    var resultChoque = this.calcChoques(personage.positionX, personage.positionY, personage.width, personage.height, world)
+                    if (resultChoque[2]) {
+                        personage.positionY += 4
+                        KeyBoardInput.state = "NONE"
+                    }
+                }
+                break;
+            case "DOWN":
+                if (!choqueScenerySuperiorBaixoY) {
+                        if (personage.positionY < (World.canvas.height - world.personage.width)) {
+                            personage.positionY += personage.speed;
+                        }
+                } 
+                break;
+            case "LEFT":
+                if (!choqueScenerySuperiorEsquerdoX){                                         
+                    if (personage.positionX > 0) {
+                        personage.positionX -= personage.speed;
+                    }
+                }
+                break;
+            case "RIGHT":
+                if (!choqueScenerySuperiorDireitoX){                                         
+                    if (personage.positionX < (World.canvas.width - world.personage.width)) {
+                        personage.positionX += personage.speed;
+                    }
+                }
+
+                break;
+        }
+    }
+
+    calcChoques(personagePositionX, personagePositionY, personageWidth, personageHeight, world){
+        let personageColumn = 0
+        let personageColumnDireita = 0
+        let personageRow = 0
+        let personageRowEmBaixo = 0
+
+        personageColumn = Math.floor((personagePositionX )/world.scenery.width);
+        personageColumnDireita = Math.floor((personagePositionX + personageWidth)/world.scenery.width);
+        personageRow = Math.floor(personagePositionY /world.scenery.height);
+        personageRowEmBaixo = Math.floor((personagePositionY + personageHeight)/world.scenery.height);
 
 
         if (personageColumn < 0) personageColumn = 0
         if (personageRow < 0) personageRow = 0
 
-        let choqueScenery = world.scenery.grid[personageRow][personageColumn] == 0 ? true : false
+        let choqueScenerySuperiorEsquerdoX = world.scenery.grid[personageRow][personageColumn] == 0 ? true : false
+        let choqueScenerySuperiorDireitoX = world.scenery.grid[personageRow][personageColumnDireita] == 0 ? true : false
+        let choqueScenerySuperiorCimaY = world.scenery.grid[personageRow][personageColumn] == 0 ? true : false
+        let choqueScenerySuperiorBaixoY = world.scenery.grid[personageRowEmBaixo][personageColumn] == 0 ? true : false
 
-        //irmaozinho da direita
-        let cenarioLeftX = (personageColumn + 1)  * (world.scenery.width)  
-
-
-        //irmaozinho da esquerda
-        let cenarioRightX = (personageColumn - 1)  * (world.scenery.width) + 60  
-        
-        //irmaozinho do meio
-        let cenarioBottomY = (personageRow - 1)  * (world.scenery.height) + 60 
-
-        let cenarioTopY = (personageRow + 1)  * (world.scenery.height)
-
-        /*
-            posicoes do personagem  
-        */
-        let personagePositionTopY = personage.positionY
-        let personagePositionBottomY = personage.positionY + personage.height
-        let personagePositionLeftX = personage.positionX
-        let personagePositionRightX = (personage.positionX) + world.personage.width
-
-        switch(KeyBoardInput.state) {
-            case "UP":    
-                if ((parseInt(world.scenery.grid[personageRow - 1 == -1 ? 0 : personageRow - 1][personageColumn]) == parseInt(0)
-                    && parseFloat(personagePositionTopY)  <= parseFloat(cenarioBottomY) 
-                    || (parseInt(world.scenery.grid[personageRow - 1 == -1 ? 0 : personageRow - 1][personageColumn + 1]) == parseInt(0) &&
-                        parseFloat(personagePositionRightX)  >= parseFloat(cenarioLeftX) && parseFloat(personagePositionTopY)  <= parseFloat(cenarioBottomY) )))  {
-                        
-                } else {
-                    if (personage.positionY >= 0) {
-                        personage.positionY -= personage.speed;
-                    }
-                }
-                break;
-            case "DOWN":
-                if ((parseInt(world.scenery.grid[personageRow + 1][personageColumn]) == parseInt(0)
-                    && parseFloat(personagePositionBottomY)  >= parseFloat(cenarioTopY)
-                    || (parseInt(world.scenery.grid[personageRow + 1][personageColumn + 1]) == parseInt(0) &&
-                    parseFloat(personagePositionRightX)  >= parseFloat(cenarioLeftX) && parseFloat(personagePositionBottomY)  >= parseFloat(cenarioTopY)
-                    ))) {
-                        
-                } else {
-                    if (personage.positionY < (World.canvas.height - world.personage.width)) {
-                        personage.positionY += personage.speed;
-                    }
-                }
-                break;
-            case "LEFT":
-                if (personage.positionX > 0) {
-                    personage.positionX -= personage.speed;
-                }
-                break;
-            case "RIGHT":
-                if (personage.positionX < (World.canvas.width - world.personage.width)) {
-                    personage.positionX += personage.speed;
-                }
-                break;
-        }
- 
-     
-    
-
-        // var text = "posicao esquerda :\n" + "1"+ "2"
-        // var text = "posicao topo :\n" + "1"+ "2"
-        // var text = "posicao direita :\n" + "1"+ "2"
-        // var text = "posicao abaixo :\n" + "1"+ "2"
-   
-      
+        return [choqueScenerySuperiorEsquerdoX, choqueScenerySuperiorDireitoX, choqueScenerySuperiorCimaY, choqueScenerySuperiorBaixoY]
     }
 
 
